9ffca275bfae7d57f4e3b746c8a22603d2ae3a9e
Anastasiia <anastasiia98@internet.ru>
2023-12-18 16:22:34 +0400

    Дз3

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+
+
+using System;
+class Program
+
+{
+    static void Main()
+    {
+        Console.Write("Введите строку для проверки на палиндром: ");
+        string inputString = Console.ReadLine();
+
+        if (IsPalindrome(inputString))
+        {
+            Console.WriteLine("Введенная строка является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Введенная строка не является палиндромом");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        string cleanedString = str.Replace(" ", "").ToLower();
+        
+        string reversedString = new string(cleanedString.Reverse().ToArray());
+
+        return cleanedString == reversedString;
+
+    }
+}
+        
+
+        
